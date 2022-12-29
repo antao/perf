35869f3040413c5e751ec6abdd54f4ad3a958b19
João <429267+antao@users.noreply.github.com>
2022-12-29 14:29:03 +0100

    Add substring vs slice benchmark

---
+++ Benchmarks/Benchmarks/SubstringVsSlice.cs
@@ -0,0 +1,20 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Benchmarks;
+
+public class SubstringVsSlice
+{
+    private string _text = null!;
+
+    [Params(10, 1000)]
+    public int CharactersCount { get; set; }
+
+    [GlobalSetup]
+    public void Setup() => _text = new string(Enumerable.Repeat('a', CharactersCount).ToArray());
+
+    [Benchmark]
+    public string Substring() => _text.Substring(0, _text.Length / 2);
+
+    [Benchmark(Baseline = true)]
+    public ReadOnlySpan<char> Slice() => _text.AsSpan().Slice(0, _text.Length / 2); // No allocation
+}
