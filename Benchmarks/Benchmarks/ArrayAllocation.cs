42106cd25d05c4fd3e892993246adc79aeb2e98b
João <429267+antao@users.noreply.github.com>
2022-12-29 14:27:37 +0100

    Add array allocation benchmark

---
+++ Benchmarks/Benchmarks/ArrayAllocation.cs
@@ -0,0 +1,35 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Benchmarks;
+
+internal struct Point
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+}
+
+[MemoryDiagnoser]
+public class ArrayAllocation
+{
+    [Benchmark]
+    public void WithNew()
+    {
+        var points = new Point[5];
+        for (int i = 0; i < 5; i++)
+        {
+            points[i].X = 5;
+            points[i].Y = 10;
+        }
+    }
+    [Benchmark]
+    public void WithStackAllocSpan() 
+    {
+        Span<Point> points = stackalloc Point[5];
+        for (int i = 0; i < 5; i++)
+        {
+            points[i].X = 5;
+            points[i].Y = 10;
+        }
+    }
+}
+
