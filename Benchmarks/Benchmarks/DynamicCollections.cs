f95f5e0c6db2fb6fae8bffe92906743c928de391
João <429267+antao@users.noreply.github.com>
2022-12-29 14:28:38 +0100

    Add dynamic collections benchmark

---
+++ Benchmarks/Benchmarks/DynamicCollections.cs
@@ -0,0 +1,29 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Benchmarks;
+
+public class DynamicCollections
+{
+    [Params(10, 1000)]
+    public int Size { get; set; }
+    
+    [Benchmark]
+    public void ListDynamicCapacity()
+    {
+        List<int> list = new List<int>();
+        for (int i = 0; i < Size; i++)
+        {
+            list.Add(i);
+        }
+    }
+    
+    [Benchmark]
+    public void ListPlannedCapacity()
+    {
+        List<int> list = new List<int>(Size);
+        for (int i = 0; i < Size; i++)
+        {
+            list.Add(i);
+        }
+    }
+}
