62021a838607e4f718045a7c308cd31b3bf8fdf7
João <429267+antao@users.noreply.github.com>
2022-12-29 14:37:11 +0100

    Move point struct out of the benchmark

--- Benchmarks/Benchmarks/ArrayAllocation.cs
+++ Benchmarks/Benchmarks/ArrayAllocation.cs
@@ -2,29 +2,24 @@
 
 namespace Benchmarks;
 
-internal struct Point
-{
-    public int X { get; set; }
-    public int Y { get; set; }
-}
-
 [MemoryDiagnoser]
 public class ArrayAllocation
 {
     [Benchmark]
     public void WithNew()
     {
-        var points = new Point[5];
+        var points = new PointStruct[5];
         for (int i = 0; i < 5; i++)
         {
             points[i].X = 5;
             points[i].Y = 10;
         }
     }
+    
     [Benchmark]
     public void WithStackAllocSpan() 
     {
-        Span<Point> points = stackalloc Point[5];
+        Span<PointStruct> points = stackalloc PointStruct[5];
         for (int i = 0; i < 5; i++)
         {
             points[i].X = 5;
