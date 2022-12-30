cb285c8b42aeb0af494bc41a93ae5793f501da7c
João <429267+antao@users.noreply.github.com>
2022-12-30 09:03:10 +0100

    Remove comment

--- Benchmarks/Benchmarks/Program.cs
+++ Benchmarks/Benchmarks/Program.cs
@@ -7,13 +7,6 @@ using Benchmarks;
 
 Console.WriteLine("Performance is a feature");
 
-// Span<T> is a struct (value type) and it will NOT cause heap allocation. Span<T> uses contiguous memory in a more convenient fashion ensuring memory and type safety.
-
-// Limitation of Span<T>
-// - Span<T> cannot be a member of a class. (because when you make it a field in a class it will be stored on the heap. This is prohibited!)
-// - Span<T> cannot be used in an async method. The result is whenever async & await are used, an AsyncMethodBuilder is created. The builder creates an asynchronous state machine, and in some situations might put parameters of the method on the heap so that Span<T> violates this rule.
-// - Span<T> cannot implement any interface
-
 var gcMode = new GcMode { Force = false };
 var config = ManualConfig
     .Create(DefaultConfig.Instance)
