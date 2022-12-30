563901025714188b1b1d0e2f3a8faa37fbc426fb
João <429267+antao@users.noreply.github.com>
2022-12-30 09:02:51 +0100

    Move comment

--- Benchmarks/Benchmarks/SubstringVsSlice.cs
+++ Benchmarks/Benchmarks/SubstringVsSlice.cs
@@ -2,6 +2,13 @@
 
 namespace Benchmarks;
 
+// Span<T> is a struct (value type) and it will NOT cause heap allocation. Span<T> uses contiguous memory in a more convenient fashion ensuring memory and type safety.
+
+// Limitation of Span<T>
+// - Span<T> cannot be a member of a class. (because when you make it a field in a class it will be stored on the heap. This is prohibited!)
+// - Span<T> cannot be used in an async method. The result is whenever async & await are used, an AsyncMethodBuilder is created. The builder creates an asynchronous state machine, and in some situations might put parameters of the method on the heap so that Span<T> violates this rule.
+// - Span<T> cannot implement any interface
+
 public class SubstringVsSlice
 {
     private string _text = null!;
